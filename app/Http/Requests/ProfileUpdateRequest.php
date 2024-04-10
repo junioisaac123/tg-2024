22bc04b8acc6bb1cf44b5ae3c0c5056fcd1a5524
junioisaac123 <96511007+junioisaac123@users.noreply.github.com>
2024-04-10 00:24:37 -0500

    fix(profileRequest): add support to new fileds in user

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,9 +17,9 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'phone' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            // 'last_name' => ['required', 'string', 'max:255'],
+            // 'username' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            // 'phone' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
