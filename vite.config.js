7e7a6e32f16fd271ea317a3cb0f1bc5a59e8001f
Isaac <jisaac197@gmail.com>
2024-10-14 15:15:52 -0500

    feat: include all js files for bundler

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
+
+// trate multiple files from js
+function gtFilesFromDir(dir, fileType) {
+    const files = fs.readdirSync(dir);
+    return files
+        .filter((file) => path.extname(file) === fileType)
+        .map((file) => path.join(dir, file));
+}
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                // 'resources/js/app.js',
+                // Import all js files from Admin
+                ...gtFilesFromDir('resources/js', '.js'),
             ],
             refresh: true,
         }),
