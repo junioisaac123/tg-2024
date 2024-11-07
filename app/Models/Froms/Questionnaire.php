d4974806624e755a5890bd3613ce511096fe98d9
Isaac <jisaac197@gmail.com>
2024-11-07 01:41:11 -0500

    feat: add Questionnaire entity

---
+++ app/Models/Froms/Questionnaire.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Froms;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Questionnaire extends Model
+{
+    use HasFactory;
+}
