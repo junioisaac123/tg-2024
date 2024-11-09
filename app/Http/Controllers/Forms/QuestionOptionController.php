eb9c89eb74d35a60074f270e8d284181898154ad
Isaac <jisaac197@gmail.com>
2024-11-09 18:53:58 -0500

    fix: ignore guards

--- app/Http/Controllers/Forms/QuestionOptionController.php
+++ app/Http/Controllers/Forms/QuestionOptionController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class QuestionOptionController extends Controller
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array<string>|bool
+     */
+    protected $guarded = [];
 }
